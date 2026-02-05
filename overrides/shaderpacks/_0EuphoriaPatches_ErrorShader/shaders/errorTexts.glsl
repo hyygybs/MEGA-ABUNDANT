171855ca732ddddbf28774b22384cd5ae8176d3c
hyygybs <94520881+hyygybs@users.noreply.github.com>
2026-02-05 17:48:06 +0800

    update

---
+++ overrides/shaderpacks/_0EuphoriaPatches_ErrorShader/shaders/errorTexts.glsl
@@ -0,0 +1,39 @@
+beginTextError(3, offset);
+    printString((_equal, _equal, _equal, _space, _S, _H, _A, _D, _E, _R, _space, _N, _O, _T, _space, _F, _O, _U, _N, _D, _space, _equal, _equal, _equal));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_R, _e, _q, _u, _i, _r, _e, _d, _colon, _space, _C, _o, _m, _p, _l, _e, _m, _e, _n, _t, _a, _r, _y, _S, _h, _a, _d, _e, _r, _s, _space, _r, _5, _dot, _7, _dot, _1));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_F, _o, _u, _n, _d, _colon, _space, _C, _o, _m, _p, _l, _e, _m, _e, _n, _t, _a, _r, _y, _U, _n, _b, _o, _u, _n, _d, _under, _r, _5, _dot, _6, _dot, _1));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_Y, _o, _u, _space, _h, _a, _v, _e, _space, _a, _n, _space, _o, _l, _d, _e, _r, _space, _v, _e, _r, _s, _i, _o, _n, _space, _i, _n, _s, _t, _a, _l, _l, _e, _d, _dot));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printLine();
+endText(textColor);
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_S, _O, _L, _U, _T, _I, _O, _N, _colon, _space, _D, _o, _w, _n, _l, _o, _a, _d, _space, _a, _n, _d, _space, _i, _n, _s, _t, _a, _l, _l, _space, _C, _o, _m, _p, _l, _e, _m, _e, _n, _t, _a, _r, _y, _S, _h, _a, _d, _e, _r, _s, _space, _r, _5, _dot, _7, _dot, _1));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_D, _o, _w, _n, _l, _o, _a, _d, _space, _f, _r, _o, _m, _colon, _space, _h, _t, _t, _p, _s, _colon, _slash, _slash, _w, _w, _w, _dot, _c, _o, _m, _p, _l, _e, _m, _e, _n, _t, _a, _r, _y, _dot, _d, _e, _v, _slash));
+endText(textColor);
+
